30a1f16a7538ca19994f216b15a505f6b1d56034
jerimkaura <jerimkaura001@gmail.com>
2022-08-15 23:53:49 +0300

    - Initial commit

--- app/src/main/java/com/jerimkaura/got/domain/usecase/ContinentsUseCase.kt
+++ app/src/main/java/com/jerimkaura/got/domain/usecase/ContinentsUseCase.kt
@@ -1,10 +1,6 @@
 package com.jerimkaura.got.domain.usecase
 
-import com.jerimkaura.got.data.local.entities.Continent
 import com.jerimkaura.got.domain.repository.ContinentsRepository
-import com.jerimkaura.got.util.Resource
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class ContinentsUseCase @Inject constructor(private val repository: ContinentsRepository){
